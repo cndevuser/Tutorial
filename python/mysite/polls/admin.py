3d3442020bc1752d76529597a0d26a0025f28d9c
cndevuser <iecwtu@163.com>
2020-05-18 12:07:45 +0800

    part2 completed

--- python/mysite/polls/admin.py
+++ python/mysite/polls/admin.py
@@ -1,3 +1,13 @@
+'''
+@Description: 注册可管理模型
+@Author: Tucw
+@Date: 2020-05-18 11:06:37
+@LastEditors: Tucw
+@LastEditTime: 2020-05-18 12:06:24
+'''
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Choice)
